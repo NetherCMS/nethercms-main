7c2042249f46114848da8d99e6c393ad39ee9d31
EletrixTime <71174682+EletrixtimeYT@users.noreply.github.com>
2023-12-12 22:27:46 +0100

    0.0.1
    a

--- config/main_config.py
+++ config/main_config.py
@@ -9,6 +9,7 @@ MAINTENANCE = False
 # WebServer
 NETHERCMS_PORT = 80 # Define port to listen
 NETHERCMS_ADDRESS = "0.0.0.0" # Define listen address
+NETHERCMS_SECRETKEY = "XJpa92qANB8jhtjN3+GyS4U}?buR9M2UVPiy7pDr82EZsAQ4}Up4vhp3,N29}Rw98y6S7.-pmKu[7-54Cf*6%kG)t)akg_tD^57462L33)JL3djcGTRe,~!}J25D2wcN9#344T4t6-[XE5xZ-5i-x3yME5@whhC+a9k]7eh9}Wg5q%L;8^5^383!wPj5@8g5P8$,(@4?S?$j?dq*6U-=i3D4e6;2@Vc82X2m!r84G9953/k#9U56p-5miYTR3GWNy;AH7#A+6p_89;DL5MP^zW^6e57Cr6Mc34T46B2+S#z#6P3d]kucW/$Na79$8.4z)}a22{)SEd?[@d7(NsXa$C#s!MNRM%4;SAj@8tn#D%XFVvDdqA5m6pC6{nKr*5Bz{iBU*23zYNuEA?=e5(49x@f/JpEH!itX8):3~2V]377wTM4gaDfc6]8P([Y6g38B9ui:69*KK%7Z2]6n^eX7u*G5Dyp]Xy$V_5pA)aHHz#a{jw3Tu8HCKrQBN7/H72.94u?R/_?a!iqg)G3%T.R3m)Dg~p@zt(G633L7JFebi74F=pABaJWJR9]TAFQ4r5E?qy75Nx6-HHbRk6?B%Z25m=yG[Upy2.2!g666~RKaG.BHxw4G$3JaE.}7-R9K5%%b,Px852TN[x6y9@29M794g}du7]urm+sFb!Ge][R_TH7NU4m32M^3{N2{4:KPw3CWs{M(_j8i2);ieV2.3V2X%9$=za2*{82$zC4)fqf7~((ze[55aQ6x8*p(!g8g65HV67]7453Jjh9gP=[xD54],Y%/6c,8wq3BjAr98P(4b8m27@m8n;5J4A7T!:}-r{RSbZg}6yhk;9ceNjNSjG^C[.w9n!RU_36z6t+-Z2t63%3@BEZ,b^zz2tGg^46n#h5~9Tik=R69+3rKmL59n{5sS/k2GsCF~4Jw,34vx7eFG2pr2U/LLbP~eD9m4)*q#2qKzzABt~8*=K36qD..Q_:(:278yekD:RB;6{$=XFCeAin4xqw7*@[Cc5s@GvfKP3wQ;#.Z:mw(sFP^v_Kg8^]qvA69W{}Z42w_Q}#d4HF5R%!x:{;+VRKgy~+9pEP*5e6^$*/3;mhDH5Hv?9KTc5Q(pvL?]4,khw#=7f6[,u~Wz:9m$hYQ28V7_bZGN6,457L+UqGB_FbTN{C7!~??geiX/CqvQVbuWp~J@37%4,5/:kP94u+)68Q}y3Ak_6$):6v.278n3+M-^eqk_;:2KjA3/B#55XXf/54dGj9t2LQn9H$85WL}C3!eQ./6D+-2*5BR=2=ufm5,a2;zWDFU(g4tW~6]bb6d+wMcLHTQCx=9ZL4,4w6[A[e-5Qb7p"
 
 # Others
 
--- main.py
+++ main.py
@@ -14,8 +14,9 @@ print(f"NetherCMS @ V{NETHERCMS_VERSION}")
 try:
     from libs import console
     from libs import telemetry
-    from flask import Flask , send_file , render_template , redirect
+    from flask import Flask , send_file , render_template , redirect , session , request , json , abort
     from datetime import datetime
+    import atexit 
 
     import os
 except Exception as e:
@@ -25,23 +26,30 @@ except Exception as e:
 # Config Import
 try:
     from config import main_config
+
 except Exception as e:
     print("Critical configuration files import failed error :")
     print(e)
 
-    
+def load_users():
+    with open("data/users.json", 'r') as file:
+        return json.load(file)
+
+def save_users(users):
+    with open("data/users.json", 'w') as file:
+        json.dump(users, file, indent=2) 
 app = Flask(__name__)
+
+app.secret_key = main_config.NETHERCMS_SECRETKEY
+
+
 @app.route('/')
 def index():
     if main_config.MAINTENANCE:
         return render_template("/error/maintenance.html",title=main_config.SERVER_TITLE,datetime_render=str(datetime.now()),logo_path=main_config.SERVER_LOGO,discord_link=main_config.DISCORD_SERVER_LINK)
     else:  
         return render_template("index.html",title=main_config.SERVER_TITLE,datetime_render=str(datetime.now()),logo_path=main_config.SERVER_LOGO,discord_link=main_config.DISCORD_SERVER_LINK,server_description=main_config.SERVER_DESCRIPTION,MC_SERVER_IP=main_config.SERVER_IP,MC_SERVER_PORT=main_config.SERVER_PORT)
-
-@app.errorhandler(404) 
-def not_found(e): 
-    return render_template("error/404.html",title=main_config.SERVER_TITLE,datetime_render=str(datetime.now()),logo_path=main_config.SERVER_LOGO,discord_link=main_config.DISCORD_SERVER_LINK) 
-
+    
 @app.route('/assets/<path:filename>')
 def get_asset(filename):
     file_path = os.path.join("assets", filename)
@@ -49,9 +57,116 @@ def get_asset(filename):
         return send_file(file_path)
     else:
         return redirect("/404")
+    
+# AUTHENFICATION PART
+@app.route('/auth/login', methods=['GET', 'POST'])
+def auth_login():
+
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        print("login")
+        users = load_users()
+
+        if username in users and users[username]['password'] == password:
+            session['username'] = username
+            return redirect('/')
+        else:
+            return("Invalid credientials")
+
+    return render_template("/auth/login.html", title=main_config.SERVER_TITLE, datetime_render=str(datetime.now()),
+                           logo_path=main_config.SERVER_LOGO, discord_link=main_config.DISCORD_SERVER_LINK,
+                           server_description=main_config.SERVER_DESCRIPTION, MC_SERVER_IP=main_config.SERVER_IP,
+                           MC_SERVER_PORT=main_config.SERVER_PORT)
+@app.route('/auth/logout')
+def auth_logout():
+    if 'username' in session:
+        session.pop('username', None)
+        return redirect('/')
+    else:
+        return("TU ES PAS CONNECTER !")
+@app.route('/auth/accountmenu')
+def auth_accountmenu():
+    if 'username' in session:
+        return render_template("/auth/accountmenu.html",title=main_config.SERVER_TITLE,datetime_render=str(datetime.now()),logo_path=main_config.SERVER_LOGO,discord_link=main_config.DISCORD_SERVER_LINK)
+    else:
+        return redirect("/auth/login")
+    
+@app.route('/auth/accountmanage')
+def auth_accountmanage():
+    if 'username' in session:
+        return render_template("/auth/accountmanage/main.html",title=main_config.SERVER_TITLE,datetime_render=str(datetime.now()),logo_path=main_config.SERVER_LOGO,discord_link=main_config.DISCORD_SERVER_LINK)
+    else:
+        return redirect("/auth/login")
+    
+@app.route('/auth/register', methods=['GET', 'POST'])
+def auth_register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        permissions = 1
+        mcuuid = 0
+        
+        users = load_users()
+
+        if username not in users:
+            console.info(f"[NetherCMS/Accounts/Register] : New user : username : {username}")
+            users[username] = {
+                'password': password,
+                'email': email,
+                'permissions': permissions,
+                'mcuuid': mcuuid,
+            
+            }
+
+            save_users(users)
+            session['username'] = username
+            
+            return render_template("others/complete.html",title=main_config.SERVER_TITLE,datetime_render=str(datetime.now()),logo_path=main_config.SERVER_LOGO,discord_link=main_config.DISCORD_SERVER_LINK)
+
+    return render_template('auth/register.html',title=main_config.SERVER_TITLE,datetime_render=str(datetime.now()),logo_path=main_config.SERVER_LOGO,discord_link=main_config.DISCORD_SERVER_LINK,server_description=main_config.SERVER_DESCRIPTION,MC_SERVER_IP=main_config.SERVER_IP,MC_SERVER_PORT=main_config.SERVER_PORT)
+
+# Admin Panel :
+
+@app.route('/admin')
+def admin_main():
+    if 'username' in session:
+        username = session['username']
+        users = load_users()
+
+        if username in users and 'permissions' in users[username] and users[username]['permissions'] == 2:
+            
+            return render_template("/admin/main.html", title=main_config.SERVER_TITLE,
+                                   datetime_render=str(datetime.now()), logo_path=main_config.SERVER_LOGO,
+                                   discord_link=main_config.DISCORD_SERVER_LINK)
+        else:
+            
+            abort(404)
+
+    else:
+        return redirect('/auth/login')
+
+# Handling error :
+
+@app.errorhandler(404) 
+def not_found(e): 
+    return render_template("error/404.html",title=main_config.SERVER_TITLE,datetime_render=str(datetime.now()),logo_path=main_config.SERVER_LOGO,discord_link=main_config.DISCORD_SERVER_LINK) 
+@app.errorhandler(500) 
+def server_error(e): 
+    return render_template("error/500.html",title=main_config.SERVER_TITLE,datetime_render=str(datetime.now()),logo_path=main_config.SERVER_LOGO,discord_link=main_config.DISCORD_SERVER_LINK,errortext=e) 
+@app.errorhandler(403) 
+def notauthorized(): 
+    return render_template("error/403.html",title=main_config.SERVER_TITLE,datetime_render=str(datetime.now()),logo_path=main_config.SERVER_LOGO,discord_link=main_config.DISCORD_SERVER_LINK) 
+
+# others
+
+
 try:
     if __name__ == '__main__':
-        app.run(debug=True,host=main_config.NETHERCMS_ADDRESS,port=main_config.NETHERCMS_PORT)
+        atexit.register(print,"Exiting...")
+
+        app.run(debug=False,host=main_config.NETHERCMS_ADDRESS,port=main_config.NETHERCMS_PORT)
 except Exception as e:
     console.alert("Failed to init webserver error :")
     console.alert(e)
